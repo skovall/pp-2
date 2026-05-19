74a45ab0d25fbfc6fd2107c5c13fd7298744659c
swede <dctsccssrslcts@gmail.com>
2026-05-19 14:58:13 +0300

    feat: добавила функцию для сложения

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
+int add(int a, int b) {
+    return a + b;
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    
+    int result = add(5, 3);
+    std::cout << "5 + 3 = " << result << std::endl;
+    
     return 0;
 }
