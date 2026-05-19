bc9ecf0877d0982b9fb4097ec8090741c3a46b24
swede <dctsccssrslcts@gmail.com>
2026-05-19 15:33:48 +0300

    feat: добавила конфигурацию

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+void loadConfig() {
+    std::string config = "config.txt";
+    std::cout << "Загрузка конфигурации из " << config << std::endl;
+}
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int multiply(int a, int b) {
+    return a * b;
+}
+
+int main() {
+    int a, b;
+    std::cout << "Введите два числа: ";
+    std::cin >> a >> b;
+    
+    std::cout << a << " + " << b << " = " << add(a, b) << std::endl;
+    std::cout << a << " * " << b << " = " << multiply(a, b) << std::endl;
+    
+    return 0;
+}
